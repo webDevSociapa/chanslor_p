e8f4eeb1e57c84ce479df1b70b5c06a733ca4e00
webDevSociapa <webdev@sociapa.com>
2025-03-27 11:20:56 +0530

    ee

---
+++ script.js
@@ -0,0 +1,24 @@
+// const slides = document.querySelector(".slides");
+// const prevBtn = document.querySelector(".prev");
+// const nextBtn = document.querySelector(".next");
+
+// let index = 0;
+// const totalSlides = document.querySelectorAll(".slide").length;
+
+// prevBtn.addEventListener("click", () => {
+//     index = (index === 0) ? totalSlides - 1 : index - 1;
+//     updateSlide();
+// });
+
+// nextBtn.addEventListener("click", () => {
+//     index = (index === totalSlides - 1) ? 0 : index + 1;
+//     updateSlide();
+// });
+
+// function updateSlide() {
+//     slides.style.transform = `translateX(-${index * 100}%)`;
+// }
+
+
+// const prevBtn1 = this.documentURI.querySelector('.prev');
+// console.log("prevBtn", prevBtn1);
